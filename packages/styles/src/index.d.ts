19792e3737cc4058284e167837dfe2ff4bc8fdfa
victordocent <victor.bostaetter@docent-art.com>
2026-02-19 16:15:02 +0100

    fix bump versions

--- packages/styles/src/index.d.ts
+++ packages/styles/src/index.d.ts
@@ -2,5 +2,7 @@ export * from "./tokens";
 /**
  * Utility function to merge Tailwind classes conditionally
  */
-export declare const cn: (...classes: (string | undefined | null | false)[]) => string;
+export declare const cn: (
+  ...classes: (string | undefined | null | false)[]
+) => string;
 //# sourceMappingURL=index.d.ts.map
